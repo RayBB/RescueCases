07593e81fc130a634a3fb258f81c68900952375d
Ray Berger <rayz321z@gmail.com>
2016-01-01 23:54:33 -0500

    update url requrement

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -3,9 +3,18 @@ class Mission < ActiveRecord::Base
 
 
 	validate :url_should_be_url
+	validate :rescued_should_be_rescued
 
   	def url_should_be_url
-    	errors.add(:rescue_link, "Not a valid URL") if
-    		!rescue_link.include?('http' || 'https') || !rescue_link.include?('imgur.com/' || 'cloudfront.net/') || !rescue_link.index('http')  == 0
+    	errors.add(:rescue_link, "Not a valid URL Please upload to imgur or cloudfront. (This is to prevent spam)") if
+    		!rescue_link.index('http')  == 0 #|| !rescue_link.include?('imgur.com/' || 'cloudfront.net/') 
   	end
+
+  	def rescued_should_be_rescued
+  		if rescued_link != ""
+  			errors.add(:rescued_link, "Not a valid URL. Please upload to imgur or cloudfront. (This is to prevent spam)") if
+    		!rescued_link.index('http')  == 0 # || !rescued_link.include?('imgur.com/' || 'cloudfront.net/')
+  		end
+    end
+
 end
