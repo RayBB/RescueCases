d198c257c1f809ff0fb37d3590680048b28d7be4
Ray Berger <rayz321z@gmail.com>
2016-01-01 20:31:06 -0500

    require valid rescue link

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,3 +1,11 @@
 class Mission < ActiveRecord::Base
 	validates :location, :floor, :rescue_link, :presence => true
+
+
+	validate :url_should_be_url
+
+  	def url_should_be_url
+    	errors.add(:rescue_link, "Not a valid URL") if
+    		!rescue_link.include?('http' || 'https') || !rescue_link.include?('imgur.com/' || 'cloudfront.net/') || !rescue_link.index('http')  == 0
+  	end
 end
